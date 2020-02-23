a23f3f8d602bd5880a82fed1b56283d8c215421a
guoxb <945201882@qq.com>
2020-02-23 20:58:00 +0800

    修改界面

--- ant-design-vue-jeecg/src/components/page/GlobalFooter.vue
+++ ant-design-vue-jeecg/src/components/page/GlobalFooter.vue
@@ -1,17 +1,12 @@
 <template>
   <div class="footer">
     <div class="links">
-      <a href="http://www.jeecg.org" target="_blank">JEECG 首页</a>
-      <a href="https://github.com/zhangdaiscott/jeecg-boot" target="_blank">
-        <a-icon type="github"/>
-      </a>
-      <a href="https://ant.design/">Ant Design</a>
-      <a href="https://vuecomponent.github.io/ant-design-vue/docs/vue/introduce-cn/">Vue Antd</a>
+      <a href="http://www.htphy.com" target="_blank">公司 首页</a>
     </div>
     <div class="copyright">
       Copyright
       <a-icon type="copyright"/>
-      2019 <span>JEECG开源社区 出品</span>
+      2020 <span>江西航天鄱湖云科技有限公司 出品</span>
     </div>
   </div>
 </template>
